ec8c73b18f8825d6a67211cc837a759297d99fe2
Paul M <paulmcadory@gmail.com>
2019-12-31 21:47:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,3 +27,13 @@ def map_to_double(source_array)
   end
   double_array
 end
+
+def map_to_square(source_array)
+  squared_array = []
+  i = 0
+  while i < source_array.length do
+    squared_array.push(source_array[i] ^^ 2)
+    i += 1
+  end
+  squared_array
+end
